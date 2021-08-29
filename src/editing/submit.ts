b6ce96bad08b15619ff068feb681025368ba3021
Oleg Valter <o.a.philippov@gmail.com>
2021-08-29 12:27:19 +0300

    improved edit submitter signature; removed dependency on Location global

--- src/editing/submit.ts
+++ src/editing/submit.ts
@@ -1,47 +1,58 @@
-    /**
-     * @summary programmatically submit an edit to the post
-     * @param {number} postId id of the post being edited
-     * @param {string} postHash hash from the edit <form> element action URL
-     * @param {string} body new post body
-     * @param {string} title new post title
-     * @param {string[]} tags list of post tags
-     * @param {string} comment edit comment
-     * @returns {Promise<boolean>}
-     */
-    export const editSubmitter = async (
-        postId: number,
-        postHash: string,
-        body: string,
-        title: string,
-        tags: string[],
-        comment: string
-    ) => {
-        try {
-            const { protocol, hostname } = location;
+export type EditSubmitter = (
+    site: `https://${string}` | `http://${string}`,
+    postId: number,
+    postHash: string,
+    body: string,
+    title: string,
+    tags: string[],
+    comment: string,
+    onError?: (msg: unknown) => void
+) => Promise<boolean>;
 
-            const res = await fetch(
-                `${protocol}//${hostname}/posts/${postId}/edit-submit/${postHash}`,
-                {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/x-www-form-urlencoded",
-                    },
-                    body: new URLSearchParams({
-                        "edit-comment": comment,
-                        "fkey": StackExchange.options.user.fkey,
-                        "is-current": "true",
-                        "post-text": body,
-                        "tagnames": tags.join(" "),
-                        title,
-                    }),
-                }
-            );
+/**
+ * @summary programmatically submit an edit to the post
+ * @param {string} site host site to submit to
+ * @param {number} postId id of the post being edited
+ * @param {string} postHash hash from the edit <form> element action URL
+ * @param {string} body new post body
+ * @param {string} title new post title
+ * @param {string[]} tags list of post tags
+ * @param {string} comment edit comment
+ * @param {(msg:unknown) => void} [onError] callback to invoke on error
+ * @returns {Promise<boolean>}
+ */
+export const submitEdit: EditSubmitter = async (
+    site,
+    postId,
+    postHash,
+    body,
+    title,
+    tags,
+    comment,
+    onError = console.log
+) => {
+    try {
+        const res = await fetch(
+            `${site}/posts/${postId}/edit-submit/${postHash}`,
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded",
+                },
+                body: new URLSearchParams({
+                    "edit-comment": comment,
+                    "fkey": StackExchange.options.user.fkey,
+                    "is-current": "true",
+                    "post-text": body,
+                    "tagnames": tags.join(" "),
+                    title,
+                }),
+            }
+        );
 
-            console.log(await res.json());
-
-            return res.status === 200;
-        } catch (error) {
-            //TODO: add handler
-            return false;
-        }
-    };
+        return res.status === 200;
+    } catch (error) {
+        onError(error);
+        return false;
+    }
+};
